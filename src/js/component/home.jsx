8f028a9797a7e3d918f44874dd2af79c4e314f56
Stefanolr04 <136249681+Stefanolr04@users.noreply.github.com>
2023-08-25 20:57:31 +0000

    Counter, no se me pone el icon

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,23 +4,25 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="bigCounter">
+			<div className="calendar">
+				<i class="fa-regular fa-clock"></i>
+			</div>
+			<div className="fourthNumber">0</div>
+			<div className="thirdNumber">0</div>
+			<div className="secondNumber">0</div>
+			<div className="firstNumber">{props.digit}</div>
+
+
 		</div>
 	);
 };
+setInterval(function(){
+
+},1000)
+
+
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,28 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+const Home = (props) => {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<i class="fa-regular fa-clock"></i>
+			</div>
+			<div className="fourthNumber">{props.digitFour % 10}</div>
+			<div className="thirdNumber">{props.digitThree  % 10}</div>
+			<div className="secondNumber">{props.digitTwo  % 10}</div>
+			<div className="firstNumber">{props.digitOne  % 10}</div>
+		</div>
+	);
+};
+let counter = 0 
+setInterval(function(){
+    const four = Math.floor(counter/1000)
+    const three = Math.floor(counter/100)
+    const two = Math.floor(counter/10)
+    const one = Math.floor(counter/1)
+    counter ++
+    ReactDOM.render(<Home digitOne={one} digitTwo={two} digitThree={three} digitFour={four}   />, 
+    document.querySelector("#app"));
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+},1000)
+
